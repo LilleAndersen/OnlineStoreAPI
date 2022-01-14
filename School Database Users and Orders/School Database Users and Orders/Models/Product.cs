fdac677c2398e8b4d3578d155e8ec5d904c6adf4
Little <71332089+LilleAndersen@users.noreply.github.com>
2022-01-14 12:13:35 +0100

    initial commit 4

--- School Database Users and Orders/School Database Users and Orders/Models/Product.cs
+++ School Database Users and Orders/School Database Users and Orders/Models/Product.cs
@@ -3,8 +3,8 @@ namespace School_Database_Users_and_Orders.Models;
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
     public float Price { get; set; }
     public int Stock { get; set; }
-    public string ImageUrl { get; set; }
+    public string ImageUrl { get; set; } = null!;
 }
